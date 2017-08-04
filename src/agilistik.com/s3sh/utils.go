5d31cf45ed005b0e3ca4e8eb1809d98eccb4214f
agilistik <info@agilistik.com>
2017-08-03 20:38:41 -0700

    Added function to get bucket and prefix.

--- src/agilistik.com/s3sh/utils.go
+++ src/agilistik.com/s3sh/utils.go
@@ -3,7 +3,10 @@ package main
 import (
 	"strings"
 )
-
+/*
+	 StrSet, NewStrSet, and Add  represent a set of strings.
+	 Currently, only 'Add' function is necessary.
+*/
 type StrSet struct {
         set map[string]bool
 }
@@ -27,6 +30,14 @@ func StringInSlice(a string, list [] string) bool {
         return false
 }
 
+/*
+	Input:  starting piont, usually pwd; and, new path.
+	Output:  the new path, if it is absolute.
+		Otherwise, starting piont + new path, with correct resolution of '.' and '..'
+	Output is an array representing the path.  It's caller's job to join it to build the path.
+	This way, the caller can use the correct  path separator if necessary.
+	Also, this provides  the ability to get any path element  (basedir, basepath, etc) at virtually no additioanl cost.
+*/
 func BuildPath (startPath, addPath string) []string  {
 	startArray	:= strings.Split(startPath, "/")
 	addArray	:= strings.Split(addPath, "/")
@@ -35,6 +46,14 @@ func BuildPath (startPath, addPath string) []string  {
 	}
 	resArray	:= make([]string, len(startArray) + len(addArray))
 	copy (resArray, startArray)
+// Remove empty strings from the beginning of resArray:
+	for i, j := range(resArray){
+		if j != "" {
+			resArray = resArray[i:]
+			break
+			
+		}
+	}	
 	addIndex := len(startArray)
 	
 	for _, j := range (addArray) {
@@ -43,7 +62,7 @@ func BuildPath (startPath, addPath string) []string  {
 				addIndex = addIndex - 1
 			}
 		} else {
-			if j != "." {
+			if j != "." && j != "" { 
 				resArray[addIndex] = j
 				addIndex = addIndex + 1
 			}
@@ -53,4 +72,14 @@ func BuildPath (startPath, addPath string) []string  {
 	
 }
 
+/*
+	Input:  path
+	Output:  bucket, prefix
+*/
+func BucketPrefix (path string) (bucket, prefix string) {
+	pathArr := BuildPath("/", path)
+	bucket = pathArr[1]
+	prefix = strings.Join(pathArr[2:], "/") + "/"
+	return bucket, prefix	
+}
 
